232e8f5c5adbd45db3505bd37f99bd86c15f470b
Dino Wernli <dinowernli@gmail.com>
2012-04-26 14:57:33 +0200

    Add ray generation to camera.

--- scene/camera.cc
+++ scene/camera.cc
@@ -4,11 +4,57 @@
 
 #include "camera.h"
 
+#include <cmath>
+
+#include "renderer/sampler/sample.h"
+
 Camera::Camera(const Point3& position, const Vector3& view, const Vector3& up,
                Scalar opening_angle, size_t resolution_x, size_t resolution_y)
-    : position_(position), view_(view), up_(up), opening_angle_(opening_angle),
+    : position_(position), opening_angle_(opening_angle),
       resolution_x_(resolution_x), resolution_y_(resolution_y) {
+  ComputeOrientation(view, up);
 }
 
 Camera::~Camera() {
+}
+
+void Camera::ComputeOrientation(const Vector3& view, const Vector3& up) {
+  view_ = view.Normalized();
+  right_ = (view_.Cross(up)).Normalized();
+  up_ = (right_.Cross(view_)).Normalized();
+}
+
+Vector3 Camera::ToWorld(const Vector3& vector) {
+  // Execute the matrix multiplication M* [vector; 1] where M =
+  // [  right.x,  right.y,  right.z,  0
+  //    up.x,     up.y,     up.z,     0
+  //    dir.x,    dir.y,    dir.z,    0
+  //    0,        0,        0,        1   ].
+  // Note that we use [vector; 1] because we are transforming a direction and
+  // not a point.
+
+  // TODO(dinow): Possibly precompute these, since they do not depend on vector.
+  Vector3 col1(right_.x(), up_.x(), view_.x());
+  Vector3 col2(right_.y(), up_.y(), view_.y());
+  Vector3 col3(right_.z(), up_.z(), view_.z());
+
+  Vector3 result = vector.x() * col1 + vector.y() * col2 + vector.z() * col3;
+  return result.Normalized();
+}
+
+Ray Camera::GenerateRay(const Sample& sample) {
+  // Add 0.5 to send ray through the pixel center.
+  Scalar image_x = sample.x() + 0.5;
+  Scalar image_y = sample.y() + 0.5;
+
+  // Convert image coordinates to camera coordinates.
+  Scalar tangent = 2 * tan(opening_angle_ * PI / 180.0);
+  Scalar factor = tangent / resolution_y_;
+
+  Scalar dx = (image_x - resolution_x_) * factor;
+  Scalar dy = (image_y - resolution_y_) * factor;
+
+  // Convert image coordinates to world coordinates.
+  Vector3 camera_coords(dx, dy, 1);
+  return Ray(position_, ToWorld(camera_coords));
 }
--- scene/camera.h
+++ scene/camera.h
@@ -10,21 +10,37 @@
 
 #include "util/numeric.h"
 #include "util/point3.h"
+#include "util/ray.h"
 #include "util/vector3.h"
+
+class Sample;
 
 class Camera {
  public:
+  // The "right" vector is inferred from "view" and "up".
   Camera(const Point3& position, const Vector3& view, const Vector3& up,
          Scalar opening_angle, size_t resolution_x, size_t resolution_y);
   virtual ~Camera();
+
+  // Returns a ray from the camera origin through sample position.
+  Ray GenerateRay(const Sample& sample);
 
   size_t resolution_x() const { return resolution_x_; }
   size_t resolution_y() const { return resolution_y_; }
 
  private:
+  // Computes a new "up" and a "right" vector such that "view", "up" and "right"
+  // are orthogonal. This guarantees that the direction of "view" is maintained.
+  void ComputeOrientation(const Vector3& view, const Vector3& up);
+
+  // Takes a vector in camera coordinates and transforms it into world
+  // coordinates.
+  Vector3 ToWorld(const Vector3& vector);
+
   Point3 position_;
-  Vector3 view_;
-  Vector3 up_;
+  Vector3 view_;  // Ensured to always be of length 1.
+  Vector3 up_;  // Ensured to always be of length 1.
+  Vector3 right_;  // Ensured to always be of length 1.
   Scalar opening_angle_;
 
   size_t resolution_x_;
