62cecb7d174e21d5c8912b3218989de2775e9c5a
Dino Wernli <dinowernli@gmail.com>
2012-04-26 09:22:09 +0200

    Add sample type.

---
+++ renderer/sampler/sample.h
@@ -0,0 +1,31 @@
+/*
+ * A class which represents a sample of an image.
+ * Author: Dino Wernli
+ */
+
+#ifndef SAMPLE_H_
+#define SAMPLE_H_
+
+#include "util/color3.h"
+#include "util/no_copy_assign.h"
+
+class Sample {
+ public:
+  // Creates a sample of size 1. The color defaults to black.
+  Sample(size_t x, size_t y) : x_(x), y_(y), size_x_(1), size_y_(1) { }
+  Sample(size_t x, size_t y, size_t size_x, size_t size_y)
+      : x_(x), y_(y), size_x_(size_x), size_y_(size_y) { }
+  ~Sample() { }
+  NO_COPY_ASSIGN(Sample);
+
+private:
+  size_t x_;
+  size_t y_;
+
+  size_t size_x_;
+  size_t size_y_;
+
+  Color3 color_;
+};
+
+#endif  /* SAMPLE_H_ */
