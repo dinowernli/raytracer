e260efe488109ffade7c32359e05873535f05db6
Dino Wernli <dinowernli@gmail.com>
2012-04-26 11:41:37 +0200

    Refactor main to use a test method.

--- raytracer.cc
+++ raytracer.cc
@@ -17,27 +17,30 @@
 #include "util/ray.h"
 #include "util/vector3.h"
 
-int main(int argc, char **argv) {
-  raytracer::Configuration config;
-  Scene scene;
-
-  std::unique_ptr<Renderer> renderer(Renderer::FromConfig(config));
-
+// This is just to test some objects.
+// TODO(dinow): Remove this when actual functionality... happens.
+void TestStuff() {
   Image image(300, 400);
-
   Vector3 vec(1, 2, 4);
   Point3 p(0, 0, 0);
 
+  raytracer::Configuration config;
+
+  std::unique_ptr<Renderer> renderer(Renderer::FromConfig(config));
+
+  Scene scene;
   Camera* cam = new Camera(p, vec, vec, 0, 300, 500);
   Light* light = new PointLight(Point3(0, 0, 0), Color3(0, 0, 0));
-
   scene.AddLight(light);
   scene.set_camera(cam);
-
   scene.Init();
 
   BmpExporter* exporter = new BmpExporter();
   renderer->AddListener(exporter);
+}
+
+int main(int argc, char **argv) {
+  TestStuff();
 
   // Free all memory in the protocol buffer library.
   google::protobuf::ShutdownProtobufLibrary();
