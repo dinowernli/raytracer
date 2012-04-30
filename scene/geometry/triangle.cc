71479ae9b24395f8d06d1af1543ee62168166ce0
Dino Wernli <dinowernli@gmail.com>
2012-04-30 09:26:05 +0200

    Add initializer of triangle for compatibility.

--- scene/geometry/triangle.cc
+++ scene/geometry/triangle.cc
@@ -7,8 +7,12 @@
 #include "renderer/intersection_data.h"
 #include "util/ray.h"
 
-Triangle::Triangle(const Point3& c1, const Point3& c2, const Point3& c3)
-    : corners_({c1, c2, c3}) {
+// TODO(dinow): Use array initializer as soon as all compilers can handler it.
+Triangle::Triangle(const Point3& c1, const Point3& c2, const Point3& c3) {
+    //: corners_({c1, c2, c3}) {
+  corners_[0] = c1;
+  corners_[1] = c2;
+  corners_[2] = c3;
 }
 
 Triangle::~Triangle() {
