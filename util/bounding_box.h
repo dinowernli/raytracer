5084aa505fc702cd78b274622c72d63969688d75
Dino Wernli <dinowernli@gmail.com>
2012-05-04 11:52:36 +0200

    Add support for dumping bounding boxes.

--- util/bounding_box.h
+++ util/bounding_box.h
@@ -22,4 +22,12 @@ class BoundingBox {
   Scalar xmin_, xmax_, ymin_, ymax_, zmin_, zmax_;
 };
 
+template<class OStream>
+OStream& operator<<(OStream& os, const BoundingBox& box) {
+  return os << "(box: "
+    "(" << box.xmin_ << ' ' << box.xmax_ << ") "
+    "(" << box.ymin_ << ' ' << box.ymax_ << ") "
+    "(" << box.zmin_ << ' ' << box.zmax_ << "))";
+}
+
 #endif  /* BOUNDING_BOX_H_ */
