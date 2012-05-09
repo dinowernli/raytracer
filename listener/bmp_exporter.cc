4194e9fce4059966a15201db9867e9164ef69538
Dino Wernli <dinowernli@gmail.com>
2012-05-09 18:05:00 +0200

    Make the BMP exporting code slightly prettier.

--- listener/bmp_exporter.cc
+++ listener/bmp_exporter.cc
@@ -21,54 +21,47 @@ void BmpExporter::Update(const Sampler& sampler) {
     return;
   }
 
-  // TODO(dinow): Move the magic constants out of this code.
-
   LOG(INFO) << "Exporting image " << file_name_;
 
   const Image& image = sampler.image();
   std::ofstream file_stream(file_name_, std::ofstream::binary);
 
-  int w = image.SizeX();
-  int h = image.SizeY();
+  const size_t width = image.SizeX();
+  const size_t height = image.SizeY();
+  const size_t filesize = 54 + 3*width*height;
 
-  int filesize = 54 + 3*w*h;
+  char file_header[14] = {'B','M',0,0,0,0,0,0,0,0,54,0,0,0};
+  char info_header[40] = {40,0,0,0,0,0,0,0,0,0,0,0,1,0,24,0};
 
-  DVLOG(1) << "Filesize: " << filesize;
+  file_header[2] = (char)(filesize);
+  file_header[3] = (char)(filesize>> 8);
+  file_header[4] = (char)(filesize>>16);
+  file_header[5] = (char)(filesize>>24);
 
-  char bmpfileheader[14] = {'B','M', 0,0,0,0, 0,0, 0,0, 54,0,0,0};
-  char bmpinfoheader[40] = {40,0,0,0, 0,0,0,0, 0,0,0,0, 1,0, 24,0};
+  info_header[4] = (char)(width);
+  info_header[5] = (char)(width>> 8);
+  info_header[6] = (char)(width>>16);
+  info_header[7] = (char)(width>>24);
+  info_header[8] = (char)(height);
+  info_header[9] = (char)(height>> 8);
+  info_header[10] = (char)(height>>16);
+  info_header[11] = (char)(height>>24);
 
-  bmpfileheader[ 2] = (char)(filesize);
-  bmpfileheader[ 3] = (char)(filesize>> 8);
-  bmpfileheader[ 4] = (char)(filesize>>16);
-  bmpfileheader[ 5] = (char)(filesize>>24);
+  file_stream.write(file_header, 14).write(info_header, 40);
 
-  bmpinfoheader[ 4] = (char)(w);
-  bmpinfoheader[ 5] = (char)(w>> 8);
-  bmpinfoheader[ 6] = (char)(w>>16);
-  bmpinfoheader[ 7] = (char)(w>>24);
-  bmpinfoheader[ 8] = (char)(h);
-  bmpinfoheader[ 9] = (char)(h>> 8);
-  bmpinfoheader[10] = (char)(h>>16);
-  bmpinfoheader[11] = (char)(h>>24);
-
-  file_stream.write(bmpfileheader, 14);
-  file_stream.write(bmpinfoheader, 40);
-
-  int extrabytes = (4 - (w * 3) % 4) % 4;
+  // Due to alignment, we must append the following number of bytes as padding.
+  const size_t extra_bytes = (4 - (width * 3) % 4) % 4;
   char padding[3] = {0, 0, 0};
 
-  for (int row = 0; row < h; ++row) {
-    for (int col = 0; col < w; ++col) {
-      const Color3& pixel = image.PixelAt(col, h - row - 1);
-      char buffer[3];
-      buffer[0] = (char)(pixel.b() * 255);
-      buffer[1] = (char)(pixel.g() * 255);
-      buffer[2] = (char)(pixel.r() * 255);
+  for (size_t row = 0; row < height; ++row) {
+    for (size_t col = 0; col < width; ++col) {
+      const Color3& pixel = image.PixelAt(col, height - row - 1);
+      char buffer[3] = { (char)(pixel.b() * 255),
+                         (char)(pixel.g() * 255),
+                         (char)(pixel.r() * 255) };
       file_stream.write(buffer, 3);
     }
-    file_stream.write(padding, extrabytes);
+    file_stream.write(padding, extra_bytes);
   }
-
   file_stream.close();
 }
