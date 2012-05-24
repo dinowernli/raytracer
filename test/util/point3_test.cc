7f3f80a944d25d3fc505d1c5c026f658a1756c47
Dino Wernli <dinowernli@gmail.com>
2012-05-24 16:57:14 +0200

    Add unit tests for point.

---
+++ test/util/point3_test.cc
@@ -0,0 +1,30 @@
+/*
+ * Unit tests for the Point3 class.
+ * Author: Dino Wernli
+ */
+
+#include <gtest/gtest.h>
+
+#include "util/point3.h"
+
+TEST(Point3, Distance) {
+  Point3 p(1, 2, 3);
+  EXPECT_DOUBLE_EQ(0, Point3::Distance(p, p));
+
+  Point3 q(1, 2, 4);
+  EXPECT_DOUBLE_EQ(1, Point3::Distance(p, q));
+
+  Point3 r(1, 2, 1);
+  EXPECT_DOUBLE_EQ(2, Point3::Distance(p, r));
+}
+
+TEST(Point3, SquaredDistance) {
+  Point3 p(1, 2, 3);
+  EXPECT_DOUBLE_EQ(0, Point3::SquaredDistance(p, p));
+
+  Point3 q(1, 2, 4);
+  EXPECT_DOUBLE_EQ(1, Point3::SquaredDistance(p, q));
+
+  Point3 r(1, 2, 1);
+  EXPECT_DOUBLE_EQ(4, Point3::SquaredDistance(p, r));
+}
