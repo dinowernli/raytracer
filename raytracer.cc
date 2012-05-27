015b28b0c03bd65c76b2910eaaaeb19913d07531
Dino Wernli <dinowernli@gmail.com>
2012-05-27 14:56:38 +0200

    Remove todo.

--- raytracer.cc
+++ raytracer.cc
@@ -73,7 +73,6 @@ DEFINE_bool(gui, false, "Whether or not to start the GLUT front end");
 TODO(dinow): Add support for building and running binaries and single tests in
              scons. Add multiple targets.
 TODO(dinow): Implement surface area heuristics for KdTree splitting
-TODO(dinow): Fix supersampling (not all identical, but distribute them in pixel
 TODO(dinow): Implement adaptive supersampling
 TODO(dinow): Add textures, perlin noise, bump maps, skyboxes, path tracing
 TODO(dinow): Add possibility to load renderer config from file.
