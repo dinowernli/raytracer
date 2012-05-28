1fe80a427124fcb5382409a99d7630458b4a49d3
Dino Wernli <dinowernli@gmail.com>
2012-05-28 14:14:12 +0200

    Rename couple variable to make code nicer.

--- renderer/sampler/supersampler.cc
+++ renderer/sampler/supersampler.cc
@@ -29,7 +29,7 @@ void Supersampler::GenerateSubsamples(const Sample& base,
 
   // Generate the evenly distributed samples.
   Scalar half_pixel = subpixel_size_ / 2;
-  size_t linear_index = 0;
+  size_t index = 0;
 
   for (size_t i = 0; i < root_num_subpixels_; ++i) {
     Scalar base_x_offset = half_pixel + i * subpixel_size_;
@@ -41,27 +41,23 @@ void Supersampler::GenerateSubsamples(const Sample& base,
       Scalar jitter_y = WillJitter() ? Random(half_pixel) : 0;
 
       // Set the offset to go through the jittered center of the subpixel.
-      Sample* current = &target->at(linear_index++);
-      *current = base;
-      current->set_offset_x(base_x_offset + jitter_x);
-      current->set_offset_y(base_y_offset + jitter_y);
-
-      DVLOG(4) << "Generating subsample " << *current << " at index "
-               << linear_index - 1;
+      Sample* sample = &target->at(index++);
+      *sample = base;
+      sample->set_offset_x(base_x_offset + jitter_x);
+      sample->set_offset_y(base_y_offset + jitter_y);
+      DVLOG(4) << "Generating subsample " << *sample << " at index " << index-1;
     }
   }
 
   DVLOG(4) << "Filling rest with random samples";
 
   // Fill the rest with completely random samples in the original pixel.
-  for(size_t i = linear_index; i < rays_per_pixel(); ++i) {
-    Sample* current = &target->at(i);
-    *current = base;
-    current->set_offset_x(Random(0.5) + 0.5);
-    current->set_offset_y(Random(0.5) + 0.5);
-
-    DVLOG(4) << "Generating subsample " << *current << " at index "
-             << i;
+  for(size_t i = index; i < rays_per_pixel(); ++i) {
+    Sample* sample = &target->at(i);
+    *sample = base;
+    sample->set_offset_x(Random(0.5) + 0.5);
+    sample->set_offset_y(Random(0.5) + 0.5);
+    DVLOG(4) << "Generating subsample " << *sample << " at index " << i;
   }
 }
 
